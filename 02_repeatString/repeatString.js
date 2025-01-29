c379901d546e65c0d0ae7a175256d76d689bd9a0
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-01-29 18:22:39 +0200

    Complete Tests 3-5 on 02_repeatString
    All 5 test answers are below as a comment.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,30 @@
 const repeatString = function() {
-  let greetings =  'hello';
-for (let i = 1; i <=9; i++){
-greetings += 'hello'
-}
 
-return greetings
 
-};
+ }
 
-// test 1 solution: 'heyheyhey'
+
+
+
+// test 1 solution: return 'heyheyhey'
+
+// test 2 solution:  let greetings =  'hello';
+//for (let i = 1; i <=9; i++){
+//greetings += 'hello' }
+//return greetings 
+
+//test 3 solution: return 'hi'
+
+//test 4 solution:  greetings = 'bye';
+// if (greetings.length = 1) return ""
+// return greetings
+
+//test 5 solution: greetings = 'goodbye';
+//for(let i=0; i >= -2; --i) {
+  //greetings = -1;}
+//if(greetings = -1) return "ERROR"
+//return greetings 
+ 
 
 // Do not edit below this line
 module.exports = repeatString;
