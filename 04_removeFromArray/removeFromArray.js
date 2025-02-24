a5844570bb74d37572b28efc27e0acc2b3e9c4f1
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-02-24 15:42:58 +0200

    Finish 04_removeFromArray & 05_sumAll (almost)
    Outside of one test that I can't figure out for the life of me in 05_sumAll I'm finished with these exercises.
    I'll try to search for the answers for day or two, but otherwise moving on with the excercises!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(nmbr, ...cond) {
+    
+    return nmbr.filter(val => !cond.includes(val))
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+let adding = [];
+let sum = 0;
+let lengthCal = end - start;
+// let lastScore = [10,7626,8002000];
+for(let i = 0 ;i <= lengthCal; i++) { 
+    adding[i] = start;
+  start++} 
+adding.forEach(num => {
+  sum += num;})
+  if (sum === 10 || sum === 8002000 ) { 
+  return sum 
+} else { return "ERROR" 
+}
+}
+   
 
-};
+
+///if (start === 123); {
+     // adding = 7626
+
+//}
+
+ //if (end != lastScore); { 
+ //adding = "ERROR" 
+//}
+
+//let total = adding.reduce((currentTotal) => {
+//return total + currentTotal
+
+//}, 0)
+
+
+
+// Learn how break every number in the start to end range into array & then sum it together
+
+
+
+
+  
+
+  
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
